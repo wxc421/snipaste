b276c47f8747d19113f0bfe32131f5a6953872f4
wxc <962447907@qq.com>
2023-06-21 19:59:35 +0800

    feat: 渲染图片

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
+use std::{fs, thread};
+use std::time::Duration;
 use sdl2::video::{Window, WindowBuilder};
 use sdl2::{Sdl, VideoSubsystem, EventPump};
 use sdl2::event::Event;
-use sdl2::pixels::Color;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
+use sdl2::surface::Surface;
+use screenshots::Screen;
+
+
+// pub fn capture<'a>() -> Surface<'a> {
+//     let screens = Screen::all().unwrap();
+//     let image = screens[0].capture().unwrap();
+//     let width = image.width();
+//     let height = image.height();
+//     let mut vec: Vec<u8> = image.into();
+//
+//     let surface = Surface::from_data(
+//         vec.as_mut_slice(),
+//         width,
+//         height,
+//         width * height * 3,
+//         PixelFormatEnum::Unknown,
+//     ).unwrap();
+//
+//     surface
+// }
 
 fn main() {
+
+    let screens = Screen::all().unwrap();
+    let image = screens[0].capture().unwrap();
+    let width = image.width();
+    let height = image.height();
+    let mut vec: Vec<u8> = image.into();
+    println!("{:?}",&vec[..160]);
+
+    // let mut buffer = image.to_png().unwrap();
+    // fs::write(format!("target/777777-{}.png", screens[0].display_info.id), buffer).unwrap();
+
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
 
     let mut window1 = create_window(&video, 0);
-    let id1 = window1.id();
-
-    // let mut canvas = window1.into_canvas().build().unwrap();
-    // canvas.set_draw_color(Color::RGBA(0, 0, 0, 100));
-    // canvas.clear();
-    // canvas.present();
-
-    let window2 = create_window(&video, 1);
-    let id2 = window2.id();
+    let id = window1.id();
 
     // 进行事件循环
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -25,11 +52,22 @@ fn main() {
     let mut start_pos = None;
     let mut end_pos = None;
 
-    let mut canvas1 = window1.into_canvas().build().unwrap();
-    let mut canvas2 = window2.into_canvas().build().unwrap();
+    let mut canvas = window1.into_canvas().build().unwrap();
 
 
-    let mut last_windows_id = 0;
+
+
+    let surface = Surface::from_data(
+        vec.as_mut_slice(),
+        width,
+        height,
+        width * 4,
+        // PixelFormatEnum::RGBA32,
+        PixelFormatEnum::BGRA32,
+    ).unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.create_texture_from_surface(surface).unwrap();
+    texture.with_lock()
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -37,13 +75,13 @@ fn main() {
                 Event::Quit { .. } => break 'main,
                 Event::Window { win_event, window_id, .. } => {
                     // 根据窗口ID判断是哪个窗口触发了事件
-                    if window_id == id1 {
+                    if window_id == id {
                         // 窗口1触发的事件
                         println!("Window 1 event: {:?}", win_event);
-                    } else if window_id == id2 {
-                        // 窗口2触发的事件
-                        println!("Window 2 event: {:?}", win_event);
                     }
+                }
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'main;
                 }
                 Event::MouseButtonDown { mouse_btn, x, y, .. } => {
                     if mouse_btn == sdl2::mouse::MouseButton::Left {
@@ -65,24 +103,66 @@ fn main() {
                 _ => {}
             }
         }
-        // 清除画布
-        canvas1.set_draw_color(Color::RGB(255, 255, 255));
-        canvas1.clear();
 
-        canvas2.set_draw_color(Color::RGB(255, 255, 255));
-        canvas2.clear();
+        // 设置绘制颜色模式为混合模式
+        canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
+        // 设置透明度
+        // canvas.set_draw_color(Color::RGBA(0, 0, 255, 128));
+        // thread::sleep(Duration::from_secs(5));
+        // canvas.window_mut().set_opacity(0.5);
+        // thread::sleep(Duration::from_secs(5));
+        // 清除画布
+        // canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+        // canvas.clear();
+
+
+        let masks = PixelFormatEnum::RGB24.into_masks().unwrap();
+
+        // let surface = Surface::from_pixelmasks(512, 512, masks).unwrap();
+
+        // println!("vec.len():{}", vec.len());
+        // println!("{}", width);
+        // println!("{}", height);
+
 
         // 绘制当前矩形
         if let (Some(start), Some(end)) = (start_pos, end_pos) {
             let rect = Rect::new(start.0, start.1, (end.0 - start.0) as u32, (end.1 - start.1) as u32);
-            canvas1.set_draw_color(Color::RGB(255, 0, 0));
-            canvas1.fill_rect(rect).unwrap();
+
+
+            canvas.set_draw_color(Color::RGBA(255, 0, 0, 128));
+            canvas.fill_rect(rect).unwrap();
+
+
+            let mut new_texture = sdl2::render::Texture::new(&canvas.texture_creator, PixelFormatEnum::RGBA8888, sdl2::render::TextureAccess::Static)
+                .map_err(|e| format!("Could not create texture: {}", e))?;
+
+            new_texture.with_lock(None, |pixels: &mut [u8], pitch: usize| {
+                let surface = background_texture.query();
+                let (width, height) = (surface.width as usize, surface.height as usize);
+                let source_pixels = background_texture.without_lock().unwrap();
+                let source_pitch = surface.pitch as usize;
+                for y in 0..selected_area_rect.height() {
+                    let source_y = selected_area_rect.y() as usize + y;
+                    let source_row = &source_pixels[(source_y * source_pitch)..((source_y + 1) * source_pitch)];
+                    let target_row = &mut pixels[(y * pitch)..((y + 1) * pitch)];
+                    let source_start = selected_area_rect.x() as usize * 4;
+                    let source_end = source_start + selected_area_rect.width() as usize * 4;
+                    let target_start = 0;
+                    let target_end = selected_area_rect.width() as usize * 4;
+                    target_row[target_start..target_end].copy_from_slice(&source_row[source_start..source_end]);
+                }
+            })?;
+
+            canvas.copy(&new_texture, None, Some(selected_area_rect))?
         }
-        canvas1.window_mut().show();
-        canvas2.window_mut().show();
+        // let src_rect = None; // 使用整个纹理
+        let dst_rect = sdl2::rect::Rect::new(100, 100, 200, 200); // 目标区域的矩形坐标
+        canvas.copy(&texture, None, None).expect("TODO: panic message");
         // 将画布显示在窗口上
-        canvas1.present();
-        canvas2.present();
+        canvas.present();
+
+        canvas.window_mut().show();
     }
 }
 
@@ -93,15 +173,16 @@ fn create_window(video: &VideoSubsystem, display_index: i32) -> Window {
     let mut window = WindowBuilder::new(
         video,
         &format!("Window {}", display_index),
-        display_bounds.width() as u32 - 200,
-        display_bounds.height() as u32 - 200,
+        display_bounds.width() as u32,
+        display_bounds.height() as u32,
     )
         .position(display_bounds.x(), display_bounds.y())
         .hidden()
+        .borderless()
         .build()
         .unwrap();
 
-    // window.set_opacity(0.5).expect("TODO: panic message");
+    // window.set_opacity(0.4).expect("TODO: panic message");
 
     window
 }
